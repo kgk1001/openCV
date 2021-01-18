b722dbd40469b100d9ac5652f3fb7fb109fc0b95
Kushantha Jayaneththie <kgk1001@outlook.com>
2021-01-18 20:49:59 +0530

    second

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import cv2
+import gtk 
+'''
+image reading
+img = cv2.imread("img/20201019_160153.jpg")
+cv2.imshow('Output',img)
+cv2.waitKey(0)
+'''
+cap = cv2.VideoCapture('[DameDesuYo] Shingeki no Kyojin (The Final Season) - 65v0 (1920x1080 10bit AAC) [1031651E].mp4')
 
+while True:
+    success, img = cap.read()
+    cv2.imshow('Video', img)
+    if 0xFF == ord('q'):
+        break
